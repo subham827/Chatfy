1d4c11dff76cfddb6afb1103f5dec60e28766343
Subham Dudheria <73893714+subham827@users.noreply.github.com>
2023-03-03 10:10:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 
+
 import './App.css';
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
@@ -7,8 +8,9 @@ import 'firebase/compat/firestore';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useFirestore } from 'react-firebase-hooks/firestore';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
-import { useState,useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useRef } from 'react';
+import { getFirestore,collection,getDocs,addDoc } from 'firebase/firestore';
 
 
 
@@ -23,13 +25,13 @@ firebase.initializeApp({
 })
 const auth = firebase.auth();
 const firestore = firebase.firestore();
+
 function App() {
   const [user] = useAuthState(auth);
   return (
    <>
 <header>
-  {/* <h1>Firechat</h1> */}
-  {/* <h2>{user ? `Welcome ${user.displayName}` : 'Please sign in'}</h2> */}
+ 
   <section>
     {user ? <ChatRoom></ChatRoom> : <SignIn></SignIn>}
   </section>
@@ -54,11 +56,12 @@ const ChatRoom = () =>{
   
   const query = messagesRef.orderBy('createdAt').limit(2500);
   const [messages] = useCollectionData(query, {idField: 'id'});
-  console.log(messages);
+  
   const [formValue, setFormValue] = useState('');
   const dummy = useRef()
   const sendMessage = async(e) => {
     e.preventDefault();
+    if(formValue.trim().length!=0){
    await messagesRef.add({
       text: formValue,
       createdAt: new Date(),
@@ -66,17 +69,27 @@ const ChatRoom = () =>{
       displayName: auth.currentUser.displayName,
       photoURL: auth.currentUser.photoURL
     })
+    console.log(messages);
+  }
     setFormValue('');
+
     dummy.current.scrollIntoView({behavior: 'smooth'});
   }
-   const playMusic = () => {
+  
+  const playMusic = () => {
     const audio = new Audio('/Ting Sound Effect.mp3');
     audio.play();
 
   }
+ 
   useEffect(()=>{
     playMusic();
+    dummy.current.scrollIntoView({ behavior: 'smooth' });
+    
+
   },[messages])
+
+   
 return (
   <>
   <SignOut></SignOut>
@@ -87,7 +100,8 @@ return (
 
 
    {messages ?( messages.map(messages => (
-      <ChatMessage key={messages.id} message={messages} messagesRef={messagesRef} />
+     
+     <ChatMessage key={messages.createdAt} message={messages} messagesRef={messagesRef} />
     ))) : <p style={{"color" : "white"}}>Chats loading 🔃 </p>}
 
     <div ref={dummy}></div>
@@ -103,15 +117,36 @@ return (
 )
 
 }
-const ChatMessage = ({message}) => {
+const ChatMessage = ({message,key}) => {
 
   
    const messageClass = message.uid === auth.currentUser.uid ? 'sent' : 'received';
+   const deleteMessage = async (uid,u) => {
+    if(u==auth.currentUser.uid){const messagesRef = firestore.collection('messages');
+    const query = messagesRef.where('createdAt', '==', uid ).get();
+    const querySnapshot = await query;
+  
+    querySnapshot.forEach((doc) => {
+      const text = doc.data().text;
+      if (text) {
+        doc.ref.delete();
+      }
+    });}
+   
+
+  
+  };
   return (
     <div className={`message ${messageClass}`}>
     
       <img src = {message.photoURL} alt="some photo"></img>
       <p>{message.text}</p>
+     
+       {auth.currentUser.uid === message.uid ?<button onClick={() => deleteMessage(message.createdAt,message.uid)}>
+                    Delete
+                  </button>: null
+      }
+      
     </div>
   )
 }
